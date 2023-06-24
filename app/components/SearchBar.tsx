eab2642723af2357252aab8e421acc1cebbf8c53
Anthony DeVenuto <adevenuto@macbook-m1.local>
2023-06-24 16:50:18 -0500

    added HeadlessUI starting to work on Search Functionality

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,16 +1,21 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
+import SearchManufacturer from './SearchManufacturer'
 
 export default function SearchBar() {
+    const [manufacturer, setManufacturer] = useState('')
     const handleSubmit = () => {
 
     }
-  return (
-    <form className="searchbar" onSubmit={handleSubmit}>
-        <div className="searchbar__item">
-            HELLO
-        </div>
-    </form>
-  )
+    return (
+        <form className="searchbar" onSubmit={handleSubmit}>
+            <div className="searchbar__item">
+                <SearchManufacturer 
+                    manufacturer={manufacturer}
+                    setManufacturer={setManufacturer}
+                />
+            </div>
+        </form>
+    )
 }
